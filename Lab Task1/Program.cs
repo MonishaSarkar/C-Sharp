4398b36a2ebb572d9c450f8f1367bdd7884b96e7
Monisha Sarkar <146477514+MonishaSarkar@users.noreply.github.com>
2023-10-10 01:07:59 +0600

    Add files via upload

---
+++ Lab Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            student s1 = new student("Monisha", "22-47063-1", "CSE", 3.25f);
+
+            s.setName("Monisha");
+            s.setId("22-47063-1");
+            s.setDepartment("CSE");
+            s.setCgpa(3.25f);
+            s.ShowInfo();
+            Console.WriteLine("--------------------------");
+            s1.ShowInfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab Task1/student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+
+namespace LabTask1
+{
+    internal class student
+    {
+        private String name;
+        private String id;
+        private String department;
+        private float cgpa;
+
+        public student(){ }
+        public student(String name, String id, String department, float cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.department = department;
+            this.cgpa = cgpa;
+        }
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return name;
+
+        }
+        public void setId(String id)
+        {
+            this.id = id;
+        }
+        public String getId()
+        {
+            return id;
+
+        }
+        public void setDepartment(String department)
+        {
+            this.department = department;
+        }
+        public String getDepartment()
+        {
+            return department;
+
+        }
+        public void setCgpa(float cgpa)
+        {
+            this.cgpa = cgpa;
+        }
+        public float getCgpa()
+        {
+            return cgpa;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine(" Name is       : " + name);
+            Console.WriteLine(" Id is         : " + id);
+            Console.WriteLine(" Department is : " + department);
+            Console.WriteLine(" Cgpa is       : " + cgpa);
+        }
+        
+    }
+}
